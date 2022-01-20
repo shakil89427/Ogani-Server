fa55844092c14e7e7a5f61ac624bc676717d4586
shakil89427 <shakilahmed89427@gmail.com>
2022-01-20 19:42:58 +0600

    contact done

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ require("dotenv").config();
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
+const { gmail } = require("googleapis/build/src/apis/gmail");
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
@@ -124,8 +127,8 @@ async function run() {
   }
 
   /* Signup */
-  app.post("/signup", async (req, res) => {
-    try {
+  try {
+    app.post("/signup", async (req, res) => {
       await client.connect();
       let user = req.body;
       const database = client.db("users");
@@ -147,16 +150,16 @@ async function run() {
         });
         res.status(200).send(token);
       }
-    } catch (error) {
-      res.send({ message: error.message });
-    } finally {
-      await client.close();
-    }
-  });
+    });
+  } catch (error) {
+    res.send({ message: error.message });
+  } finally {
+    await client.close();
+  }
 
   /* Login */
-  app.post("/login", async (req, res) => {
-    try {
+  try {
+    app.post("/login", async (req, res) => {
       await client.connect();
       const database = client.db("users");
       const users = database.collection("allusers");
@@ -178,12 +181,48 @@ async function run() {
       } else {
         res.sendStatus(401);
       }
-    } catch (error) {
-      res.send({ message: error.message });
-    } finally {
-      await client.close();
-    }
-  });
+    });
+  } catch (error) {
+    res.send({ message: error.message });
+  } finally {
+    await client.close();
+  }
+
+  /* Send Email for contact use */
+  try {
+    app.post("/sendemail", async (req, res) => {
+      const data = req.body;
+      const oAuth2Client = new google.auth.OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URI
+      );
+      oAuth2Client.setCredentials({
+        refresh_token: process.env.REFRESH_TOKEN,
+      });
+      const accessToken = await oAuth2Client.getAccessToken();
+      const transport = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: "shakilahmed89427@gmail.com",
+          clientId: process.env.CLIENT_ID,
+          clientSecret: process.env.CLIENT_SECRET,
+          refreshToken: process.env.REFRESH_TOKEN,
+          accessToken: accessToken,
+        },
+      });
+      const response = await transport.sendMail({
+        from: data.email,
+        to: "shakilahmed89428@gmail.com",
+        subject: data.name,
+        text: data.message,
+      });
+      res.send(response);
+    });
+  } catch (error) {
+    res.send({ message: error.message });
+  }
 }
 run().catch(console.dir);
 
